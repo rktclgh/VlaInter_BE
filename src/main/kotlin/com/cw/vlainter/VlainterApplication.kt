9ab69ac3aca1393bf033b6b0eda8ac1d56aca856
rktclgh <songchih@icloud.com>
2026-02-03 21:53:27 +0900

    first commit

---
+++ src/main/kotlin/com/cw/vlainter/VlainterApplication.kt
@@ -0,0 +1,11 @@
+package com.cw.vlainter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VlainterApplication
+
+fun main(args: Array<String>) {
+	runApplication<VlainterApplication>(*args)
+}
---
+++ src/test/kotlin/com/cw/vlainter/VlainterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cw.vlainter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VlainterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
