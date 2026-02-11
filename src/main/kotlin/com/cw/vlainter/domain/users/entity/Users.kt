9a38a5baf81fa122684e2aaf3cbb10d750049317
younsoo <younsoo6103@naver.com>
2026-02-11 21:19:29 +0900

    [feat] Users Entity 구현

--- src/main/kotlin/com/cw/vlainter/domain/users/entity/Users.kt
+++ src/main/kotlin/com/cw/vlainter/domain/users/entity/Users.kt
@@ -15,24 +15,35 @@ class Users (
     val email: String,
 
     @Column(nullable = false)
-    val password: String,
+    var password: String,
 
     @Column(nullable = false, length = 100)
-    val name: String,
+    var name: String,
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    val status: UserStatus = UserStatus.ACTIVE,
+    var status: UserStatus = UserStatus.ACTIVE,
 
     @Column(nullable = false)
-    val free: Int = 0,
+    var free: Int = 0,
 
     @Column(nullable = false)
-    val point: Long = 0,
+    var point: Long = 0,
 
     @Column(name = "created_at", nullable = false)
-    val createdAt: OffsetDateTime = OffsetDateTime.now(),
+    val createdAt: OffsetDateTime? = null,
 
     @Column(name = "updated_at", nullable = false)
-    val updatedAt: OffsetDateTime = OffsetDateTime.now()
-)
+    var updatedAt: OffsetDateTime? = null
+){
+    @PrePersist
+    fun prePersist() {
+        createdAt = OffsetDateTime.now()
+        updatedAt = OffsetDateTime.now()
+    }
+
+    @PreUpdate
+    fun preUpdate() {
+        updatedAt = OffsetDateTime.now()
+    }
+}
